0db7a745b7e00f29cf87b77fe49cb4554933db68
BenziDev <benzidev@protonmail.com>
2021-01-03 20:01:11 -0700

    boop

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,6 +2,9 @@ import { Router } from 'express';
 import { Client } from 'pg';
 import Joi from 'joi';
 import logger from 'jethro';
+import argon2, { argon2id } from 'argon2';
+import intformat from 'biguint-format';
+import FlakeId from 'flake-idgen';
 
 export default (db:Client) => {
 
@@ -19,7 +22,21 @@ export default (db:Client) => {
 
       if (response.rows[0]) return res.status(400).json({"statusCode":400,"error":"Email already used"});
 
-      // continue here
+      const response2 = await db.query("SELECT * FROM users WHERE users.username = $1", [req.body.username]);
+
+      if (response2.rows[0]) return res.status(400).json({"statusCode":400,"error":"Username already used"});
+
+      const FlakeGen = new FlakeId();
+
+      const id = intformat(FlakeGen.next(), 'dec');
+
+      const hash = await argon2.hash(req.body.email, {type: argon2id, saltLength: 45});
+
+      logger("debug", "test", parseInt(id));
+
+      await db.query("INSERT INTO users (id,username,email,hash) VALUES ($1,$2,$3,$4);", [id, req.body.username, req.body.email, hash]);
+
+      return res.status(200).json({"statusCode":200,"message":"successfully created user"});
 
     } catch (err) {
       logger("error", "Postgres", err);
@@ -33,7 +50,7 @@ export default (db:Client) => {
 
 // register request validation schema
 const registerSchema = Joi.object({
-  username: Joi.string().min(3).max(20).required(),
+  username: Joi.string().min(3).max(20).required().regex(/^[a-zA-Z0-9]+$/),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).max(40).required()
 });
